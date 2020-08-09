9764abc0a663cc20d1e9c347fe9665a9cc0b4510
danhtan <45035071+danhtan@users.noreply.github.com>
2020-08-09 16:07:44 +0700

    update

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 const express = require("express");
 const path = require('path');
-const mysql = require("mysql");
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './env'});
+dotenv.config({ path: './env' });
 
 const app = express();
-
-const db = mysql.createConnection({
-  host: 'bqon1oszrjlhehhaz0dd-mysql.services.clever-cloud.com',
-  user: 'ujocmioalprjj9jo',
-  password: '99oiuibi5jLmPJbOrl0r',
-  database: 'bqon1oszrjlhehhaz0dd'
-});
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
-
-db.connect( (error) => {
-if(error){
-  console.log(error)
-}else{
-  console.log("MYSQL Connected....")
-}
-
-})
-app.use('/' , require('./routes/pages'));
-app.use('/auth' , require('./routes/auth'));
-app.listen(process.env.PORT || 8080,() =>{
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server run on 8080.");
 })
